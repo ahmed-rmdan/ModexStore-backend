28d3d2e67ccfa6b453a4a98a95deaed6e6c1ec27
ahmed-rmdan <ahmedrmadan251998@gmail.com>
2025-08-17 14:07:31 +0300

    project ready

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-console.log('hellooo')
+import  express  from "express";
+import dotenv from 'dotenv'
+import helmet from "helmet";
+import { setheaders } from "./middleware";
+
+
+dotenv.config()
+
+
+const app=express()
+
+app.use(helmet())
+app.use(setheaders)
+
+
+app.use(express.json())
+
+app.listen(3000)
---
+++ src/middleware.ts
@@ -0,0 +1,10 @@
+import { Request, Response, NextFunction } from 'express';
+export const setheaders=(req:Request,res:Response,next:NextFunction)=>{
+    res.setHeader('Access-Control-Allow-Origin','*')
+    res.setHeader('Access-Control-Allow-Methods','GET,POST,DELETE,PUT,PATCH')
+    res.setHeader('Access-Control-Allow-Headers','Content-Type,Authorization')
+    if (req.method === 'OPTIONS') {
+    return res.sendStatus(200); 
+  }
+    next()
+}
