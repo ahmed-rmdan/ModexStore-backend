c1d8731fa5df44dfeeef22db90a7163289f68404
ahmed-rmdan <ahmedrmadan251998@gmail.com>
2025-10-02 13:52:39 +0300

    add document api

--- src/index.ts
+++ src/index.ts
@@ -6,17 +6,19 @@ import { createHandler   } from 'graphql-http/lib/use/express';
 import { resolver } from './graphql/resolver'
 import { schema } from './graphql/schema'
 import dotenv from 'dotenv'
-import fs from 'fs'
-
 import { PrismaClient } from '@prisma/client'
-
 import { Request } from 'express';
 import jwt from 'jsonwebtoken'
 import Stripe from 'stripe';
-import { Server, Socket } from 'socket.io';
+import { Server} from 'socket.io';
 import { JwtPayload } from 'jsonwebtoken';
 import {resetpass} from './restapi'
 import { confirmnewpass } from './restapi';
+const swaggerJsDoc=require('swagger-jsdoc')
+const swaggerUI=require('swagger-ui-express')
+import { express as voyagerMiddleware } from "graphql-voyager/middleware";
+
+
 
 const prisma=new PrismaClient()
 dotenv.config();
@@ -28,19 +30,25 @@ cloudinary.config({
 
 })
 
-// const storge=multer.diskStorage({
-//   destination(req, file, callback) {
-//     callback(null,'upload/')
-//   },
-//   filename(req, file, callback) { 
-//     callback(null,Date.now()+'-'+file.originalname)
-//   },
-// })
+
 const storge = multer.memoryStorage();
 
 const upload=multer({storage:storge,limits:{ fileSize: 10 * 1024 * 1024 }})
 
 const app = express()
+
+const swaggerOptions={
+  swaggerDefinition:{
+    info:{
+      title:'library API',
+      version:'1.0.0'
+    }
+   
+  } ,
+  apis:["./src/**/*.ts"]
+}
+const swaggerDocs=swaggerJsDoc(swaggerOptions)
+app.use('/api-docs',swaggerUI.serve,swaggerUI.setup(swaggerDocs))
 
 
 
@@ -61,7 +69,7 @@ app.use(express.json({ limit: "10mb" }));
 app.post('/uploadimge/:productid',upload.single('mainimge'),async (req,res)=>{
   try{
  const id=req.params.productid
- console.log(id)
+ 
  if(!id){
   res.status(401).send({message:'productid not found'})
  }
@@ -86,7 +94,7 @@ app.post('/uploadimge/:productid',upload.single('mainimge'),async (req,res)=>{
 
 
     }catch(err){
-          console.log(err)
+         
            res.status(401).send({message:'upload failed'})
     }
   
@@ -94,12 +102,12 @@ app.post('/uploadimge/:productid',upload.single('mainimge'),async (req,res)=>{
 })
 
 const stripe=new Stripe(process.env.stripe_secret_key as string)
-console.log(process.env.stripe_secret_key)
+
 
 app.post('/stripecheck',async (req,res)=>{
-  console.log('stripe')
+ 
    const token=req.headers.authorization?.split(' ')[1]
-   console.log(token)
+ 
  if(!token){
   return res.status(401).send({message:'not authorized'})
        }
@@ -115,7 +123,7 @@ app.post('/stripecheck',async (req,res)=>{
            }
 
           const items:{productid:string,quantity:number}[]= await req.body
-             console.log(items)
+             
                      const productsid= items.map(elm=>{
                                 return elm.productid
                                })
@@ -136,7 +144,7 @@ app.post('/stripecheck',async (req,res)=>{
                                 success_url:'https://modexstore-backend.onrender.com/stripesucess',
                                 cancel_url:'https://modexstore-backend.onrender.com/stripecancel'
                               })
-                              console.log(payment.url)
+                             
                      return res.status(200).json(payment.url)                 
 
 })
@@ -171,7 +179,7 @@ app.all('/graphql', createHandler({ schema, rootValue: resolver, context:(req:Re
   
   }) )
 
-
+app.use("/voyager", voyagerMiddleware({ endpointUrl: "/graphql" }));
 
 let io:Server;
 
@@ -187,10 +195,9 @@ const dectoken= jwt.verify(token,'veryverysecret') as JwtPayload
 const userid:string=dectoken.userid 
 
 socket.on('joinuserroom',async ()=>{
-  console.log(socket.id)
+
 socket.join(userid)
-console.log(userid)
-console.log(socket.rooms)
+
 })
 socket.on('disconnect',()=>{
 
